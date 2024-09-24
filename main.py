c786ec37622d2f48c96679b54912a8e0b29f3983
raymondwusuper <raymond.wu.super@gmail.com>
2024-09-24 16:01:34 -0700

    Update main.py

--- main.py
+++ main.py
@@ -22,6 +22,7 @@ def add_points(transaction: AddPointsRequest):
     transactions.append(transaction)
     balances[transaction.payer] += transaction.points
     if balances[transaction.payer] < 0: 
+        balances[transaction.payer] -= transaction.points
         raise HTTPException(status_code=400, detail="Negative balance not allowed for any payer.")
     return {"status": "Points added successfully"}
 
